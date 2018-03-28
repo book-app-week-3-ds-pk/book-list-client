cb3b1d7a727c0590a251d8ce5f1b792119737545
dahby <dstoll243@gmail.com>
2018-03-28 12:07:58 -0700

    routes.js built

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -4,8 +4,32 @@ var app = app || {};
 
 (function(module){
   var bookView={};
-  bookView.initIndexPage= function(){$('.book-view').hide()
+  bookView.initIndexPage= function(){
+    $('#book-view').show();
+    $('#about-view').hide();
+    $('#detail-view').hide();
+    $('#form-view').hide();
     module.Book.all.map(book => {$('#book-view').append(book.toHtml())})
+  }
+
+  bookView.initDetailView = function(){
+    $('#book-view').hide();
+    $('#about-view').hide();
+    $('#detail-view').show();
+    $('#form-view').hide();
+  }
+
+  bookView.initAboutView=function(){
+    $('#book-view').hide();
+    $('#about-view').show();
+    $('#detail-view').hide();
+    $('#form-view').hide();
+  }
+  bookView.initFormView=function(){
+    $('#book-view').hide();
+    $('#about-view').hide();
+    $('#detail-view').hide();
+    $('#form-view').show();
   }
 
   module.bookView = bookView;
